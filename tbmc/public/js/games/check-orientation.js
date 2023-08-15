75e89c8204ed17a8b90de14c51f90b4adba9ae57
injWang <a271954@gmail.com>
2023-08-15 18:11:01 +0800

    使用手機時也可以進入頁面

--- tbmc/public/js/games/check-orientation.js
+++ tbmc/public/js/games/check-orientation.js
@@ -1,24 +1,31 @@
+var haveGetUsePadNotice = false;
 function applyDeviceCheck() {
 
     if (
-        navigator.userAgent.match(/Android/i) || 
+        (navigator.userAgent.match(/Android/i) || 
         navigator.userAgent.match(/webOS/i)   || 
         navigator.userAgent.match(/iPhone/i)  || 
         navigator.userAgent.match(/iPod/i)    || 
         navigator.userAgent.match(/BlackBerry/i) || 
-        navigator.userAgent.match(/Windows Phone/i)
+        navigator.userAgent.match(/Windows Phone/i))
+        && !haveGetUsePadNotice
     ) {
         setTimeout(function(){
             Swal.fire({
-                title: '請使用平板或電腦',
+                title: '建議使用平板或電腦獲得最佳體驗',
                 width: '90%',
                 imageUrl: useComputerOrPadImgUrl,
                 imageWidth:250,
                 showCancelButton: false,
-                showConfirmButton: false,
+                showConfirmButton: true,
                 allowOutsideClick: false,
                 allowEscapeKey:false,
                 reverseButtons:true,
+            }).then(function(result) {
+                if (result.isConfirmed) {
+                    haveGetUsePadNotice=true;
+                    applyDeviceCheck();
+                }
             });
         }, 100);
     }else if (window.innerHeight > window.innerWidth) {
