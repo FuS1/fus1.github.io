9fff4f0061e2656167f52505940a859e5d69ba25
injwang <a271954@gmail.com>
2024-10-11 00:02:26 +0800

    init

---
+++ hualein/js/common.js
@@ -0,0 +1,25 @@
+let header = "_component/header.html";
+let footer = "_component/footer.html";
+
+const requestHeader = axios.get(header);
+const requestFooter = axios.get(footer);
+
+axios.all([requestHeader, requestFooter]).then(axios.spread((...responses) => {
+    $(".container-fluid.header").html(responses[0].data);
+    $(".container-fluid.footer-sec").html(responses[1].data);
+
+    // 導覽列
+    $('.mb-navi-btn').click(function() {
+        $('.mb-navi-btn').toggleClass('active');
+        $('.navi-bar').toggleClass('active');
+    });
+
+    // 導覽列子項目
+    $('.navi-list.dropdown').click(function() {
+        $('.navi-list.dropdown').removeClass('active');
+        $(this).addClass('active');
+    });
+
+})).catch(errors => {
+    console.error('Loading components failed...');
+});
